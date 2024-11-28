f30639a0b7c3f62ff97d13a21f39889e53c496e6
sanchz16 <edsanchzs1611@gmail.com>
2024-11-28 12:13:29 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -17,7 +17,17 @@ criaCartao(
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
